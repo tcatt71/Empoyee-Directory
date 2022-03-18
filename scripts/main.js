61aea98ccf24001cd0411f4e6be7ab0e0a43ef2b
Troy Coffindaffer <61867285+tcatt71@users.noreply.github.com>
2022-03-18 16:14:52 -0700

    Style profile cards

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,13 @@ const cards = document.querySelectorAll('.card');
 
 fetch('https://randomuser.me/api/?results=12')
   .then(Response => Response.json())
+  // .then(data => console.log(data))
   .then(data => populateCards(data));
 
 function populateCards(data) {
   for (let i = 0; i < data.results.length; i++) {
     const cardHtml = `
-      <img src="${data.results[i].picture.thumbnail}">
+      <img src="${data.results[i].picture.medium}">
       <address>
         <p class="employee-name">${data.results[i].name.first} ${data.results[i].name.last}</p>
         <a href="mailto:${data.results[i].email}">${data.results[i].email}</a>
